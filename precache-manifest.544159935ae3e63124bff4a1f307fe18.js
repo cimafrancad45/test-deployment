524170637670d02b8d63309007b3c33210f83266
Daryl Cimafranca <cimafrancad45@gmail.com>
2018-12-13 13:51:41 -0500

    Updates

---
+++ precache-manifest.544159935ae3e63124bff4a1f307fe18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e61d1459845bcc13a0ec",
+    "url": "/static/js/main.e61d1459.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "e61d1459845bcc13a0ec",
+    "url": "/static/css/main.d844f477.chunk.css"
+  },
+  {
+    "revision": "bd807e5ed671e5ec917fb6ffcecfd3bc",
+    "url": "/index.html"
+  }
+];
